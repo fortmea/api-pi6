74e3f616fd2b28e06d69ea91d0a4a6d82a554a8d
fortmea <joao.amadeu@unifio.edu.br>
2022-09-15 00:04:48 +0000

    Nova query pesquisa nome

--- pi6/src/main/java/tech/fortmea/pi6/controllers/PlantaController.java
+++ pi6/src/main/java/tech/fortmea/pi6/controllers/PlantaController.java
@@ -15,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tech.fortmea.pi6.model.Imagem;
 import tech.fortmea.pi6.model.Planta;
 import tech.fortmea.pi6.model.ReqDTO;
+import tech.fortmea.pi6.repository.ImagemRepository;
 import tech.fortmea.pi6.repository.PlantaRepository;
 
 @RestController
@@ -25,6 +27,8 @@ import tech.fortmea.pi6.repository.PlantaRepository;
 public class PlantaController {
     @Autowired
     PlantaRepository plantaRepository;
+    @Autowired
+    ImagemRepository imgRepo;
 
     @GetMapping("/")
     public ResponseEntity<List<Planta>> inicio() {
@@ -39,7 +43,13 @@ public class PlantaController {
 
     @DeleteMapping("/remover")
     public ResponseEntity<String> remover(@RequestBody ReqDTO req) {
+        List<Imagem> imagens = new ArrayList<Imagem>();
+        imagens = imgRepo.findAllByPlantaId(req.getId());
+        for (int i = 0; i < imagens.size(); i++) {
+            imgRepo.deleteById(imagens.get(i).getId());
+        }
         plantaRepository.deleteById(req.getId());
+
         return ResponseEntity.ok("Removido com sucesso!");
     }
 
@@ -53,17 +63,36 @@ public class PlantaController {
         }
     }
 
-    @GetMapping("/nome/{nome}")
-    public List<Planta> retornaPlantaNome(@PathVariable String nome){
-        try{
-           return plantaRepository.findAllByNomeContainingOrNomeCientificoContaining(nome, nome);
-        }catch(Exception e){
+    @GetMapping("/nomep/{nome}")
+    public List<Planta> retornaPlantaNomePopular(@PathVariable String nome) {
+
+        try {
+            return plantaRepository.findByNomeIn(nome);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return new ArrayList<Planta>();
         }
     }
+
+    @GetMapping("/nome/{nome}")
+    public List<Planta> retornaPlantaNome(@PathVariable String nome) {
+        List<Planta> Plantas = new ArrayList<Planta>();
+        try {
+            Plantas = plantaRepository.findDistinctByNomeCientificoContaining(nome);
+            List<Planta> pPop = plantaRepository.findByNomeIn(nome.toLowerCase());
+            for (int i = 0; i < pPop.size(); i++) {
+                Plantas.add(pPop.get(i));
+            }
+
+            return Plantas;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ArrayList<Planta>();
+        }
+    }
+
     @PatchMapping("/atualizar")
-    public ResponseEntity<String> atualizaPlanta(@RequestBody Planta planta){
+    public ResponseEntity<String> atualizaPlanta(@RequestBody Planta planta) {
         plantaRepository.save(planta);
         return ResponseEntity.ok("Atualizado com sucesso!");
     }
--- pi6/src/main/java/tech/fortmea/pi6/repository/PlantaRepository.java
+++ pi6/src/main/java/tech/fortmea/pi6/repository/PlantaRepository.java
@@ -3,11 +3,17 @@ package tech.fortmea.pi6.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import tech.fortmea.pi6.model.Planta;
 
 @Repository
 public interface PlantaRepository extends JpaRepository<Planta, Long>{
-    List<Planta> findAllByNomeContainingOrNomeCientificoContaining(String nome, String nomeCientifico);
+    List<Planta> findDistinctByNomeCientificoContaining(String nomeCientifico);
+    
+    @Query("SELECT p FROM Planta p WHERE (LOWER(:Pnome)) MEMBER p.nome")
+    List<Planta> findByNomeIn(@Param("Pnome") String nome);
+    
 }
